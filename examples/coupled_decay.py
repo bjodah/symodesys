5c3816d2b09a4d4677ae501714c3559a1467c182
Björn Dahlgren <bjodah@gmail.com>
2013-02-05 16:48:53 +0100

    Updated coupled decay example to show errors

--- examples/coupled_decay.py
+++ examples/coupled_decay.py
@@ -4,11 +4,15 @@
 import sys
 
 import sympy
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 from symodesys.firstorder import FirstOrderODESystem
 from symodesys.integrator import SciPy_IVP_Integrator
+
+# TODO
+# Implement automatic resolution of N - number of chained decays via
+# Bateman's equations
 
 class CoupledDecay(FirstOrderODESystem):
 
@@ -29,7 +33,7 @@ class CoupledDecay(FirstOrderODESystem):
                 ]
 
 
-def main(lambda_u, labmda_v):
+def main(lambda_u, lambda_v):
     """
     """
     cd = CoupledDecay()
@@ -43,14 +47,33 @@ def main(lambda_u, labmda_v):
     t0 = 0.0
     tend = 10.0
     u0 = 1.0
-    v0 = 1.0
+    v0 = 0.0
 
     intr.integrate([u0, v0], t0, tend, N, **int_kwargs)
-    intr.plot(interpolate = True)
 
     t = np.linspace(t0, tend, N)
-    analytic_u = np.exp(-k*t)
-    #analytic_v = np.exp(-k*t)
+    analytic_u = np.exp(-lambda_u*t)
+    analytic_v = u0 * lambda_u / (lambda_v - lambda_u) * (np.exp(-lambda_u*t) - \
+                                                       np.exp( - lambda_v * t))
+
+
+    plt.subplot(311)
+    intr.plot(interpolate = True, show = False)
+
+    plt.subplot(312)
+    plt.plot(intr.tout, (intr.yout[:, 0] - analytic_u) / int_kwargs['abstol'],
+             label = 'u abserr / abstol')
+    plt.plot(intr.tout, (intr.yout[:, 1] - analytic_v) / int_kwargs['abstol'],
+             label = 'v abserr / abstol')
+    plt.legend()
+    plt.subplot(313)
+    plt.plot(intr.tout, (intr.yout[:, 0] - analytic_u) / analytic_u / int_kwargs['reltol'],
+             label = 'u relerr / reltol')
+    plt.plot(intr.tout, (intr.yout[:, 1] - analytic_v) / analytic_v / int_kwargs['reltol'],
+             label = 'v relerr / reltol')
+    plt.legend()
+    plt.show()
+
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(float(sys.argv[1]), float(sys.argv[2]))
